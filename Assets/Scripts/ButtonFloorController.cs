0f770f519ab99db840229636e82d50e861a5a009
3kije <martahyjek043@gmail.com>
2021-06-03 18:40:31 +0200

    button floors and next lvls

---
+++ Assets/Scripts/ButtonFloorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFloorController : MonoBehaviour
+{
+    
+    public GameObject floor;
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+       floor.GetComponent<movingObjectController>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player 1") || collision.CompareTag("Player 2"))
+        {
+            floor.GetComponent<movingObjectController>().enabled = true;
+            audio.Play();
+        }
+    }
+}
