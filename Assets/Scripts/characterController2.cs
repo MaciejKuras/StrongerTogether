490fbd70c7c75c71afc729a132cb6ea56571d1bf
さめはだ <mkuras269@gmail.com>
2021-06-06 16:00:01 +0200

    final version

--- Assets/Scripts/characterController2.cs
+++ Assets/Scripts/characterController2.cs
@@ -4,7 +4,9 @@ public class characterController2 : MonoBehaviour
 {
     float speed = 1f, checkRadius = 0.2f, jumpForce = 20f;
     public bool isGrounded;
+    public bool isUnderBlock;
     public Transform feetPos;
+    public Transform headPos;
     public LayerMask whatIsGround;
     public Animator animator;
     public AudioSource audio;
@@ -74,54 +76,12 @@ public class characterController2 : MonoBehaviour
             collider.offset = new Vector2(collider.offset.x, -0.3f);
             animator.SetBool("IsCrouching", false);
         }
-        //if (Input.GetAxisRaw("P2Horizontal") == Input.GetAxisRaw("P2Horizontal")/* && Input.GetAxisRaw("P2Horizontal") != 0*/)
-        //{
-        //    //transform.position += new Vector3(moveInput, 0, 0) * Time.deltaTime * speed * 10f;
-        //    rb.velocity = new Vector2(moveInput * speed * 10f, rb.velocity.y);
-        //    if (moveInput > 0)
-        //    {
-        //        transform.eulerAngles = new Vector3(0, 0, 0);
-        //    }
-        //    else if (moveInput < 0)
-        //    {
-        //        transform.eulerAngles = new Vector3(0, 180, 0);
-        //    }
-
-        //    animator.SetFloat("Speed", Mathf.Abs(moveInput));
-        //}
-        //else
-        //{
-        //    rb.velocity = new Vector2(0, rb.velocity.y);
-        //}
-
-        //isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-        //if (isGrounded && Input.GetButton("P2Jump") && Input.GetButtonDown("P2Jump") || isGrounded && Input.GetButtonDown("P2Jump") && Input.GetButton("P2Jump"))
-        //{
-        //    rb.velocity = Vector2.up * jumpForce;
-        //    animator.SetBool("IsJumping", true);
-        //    audio.Play();
-        //}
-
-        //if (Physics2D.OverlapCircle(feetPos.position, checkRadius).CompareTag("Floor"))
-        //{
-        //    gameObject.transform.parent = Physics2D.OverlapCircle(feetPos.position, checkRadius).transform;
-        //    //GetComponent<CapsuleCollider2D>().sharedMaterial.friction = 1;
-        //}
-        //else
-        //{
-        //    gameObject.transform.parent = null;
-        //    //GetComponent<CapsuleCollider2D>().sharedMaterial.friction = 0;
-        //}
-
-        //if (rb.velocity.y < 0 && !isGrounded)
-        //{
-        //    animator.SetBool("IsJumping", false);
-        //    animator.SetBool("IsFalling", true);
-        //}
-        //else/* if (rb.velocity.y >= 0)*/
-        //{
-        //    animator.SetBool("IsFalling", false);
-        //    //animator.SetBool("IsJumping", false);
-        //}
+        isUnderBlock = Physics2D.OverlapCircle(headPos.position, checkRadius, whatIsGround);
+        if (isUnderBlock)
+        {
+            collider.size = new Vector2(collider.size.x, 0.5f);
+            collider.offset = new Vector2(collider.offset.x, -0.55f);
+            animator.SetBool("IsCrouching", true);
+        }
     }
 }
