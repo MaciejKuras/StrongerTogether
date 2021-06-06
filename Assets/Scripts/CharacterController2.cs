dc758c0efbbd133c3046cd07d29787dc999d1795
3kije <martahyjek043@gmail.com>
2021-06-06 15:49:19 +0200

    player12

--- Assets/Scripts/CharacterController2.cs
+++ Assets/Scripts/CharacterController2.cs
@@ -1,37 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController2 : MonoBehaviour
+public class characterController2 : MonoBehaviour
 {
     float speed = 1f, checkRadius = 0.2f, jumpForce = 20f;
     public bool isGrounded;
+    public bool isUnderBlock;
     public Transform feetPos;
+    public Transform headPos;
     public LayerMask whatIsGround;
     public Animator animator;
     public AudioSource audio;
     Rigidbody2D rb;
-
-    public Collider2D crouchDisableCollider;
-    public Transform ceilingCheck;
-    const float ceilingRadius = 0.2f;
-    private bool isCrouching = false;
-    private bool isUnderCeiling;
+    CapsuleCollider2D collider;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         audio = GetComponent<AudioSource>();
-
-        crouchDisableCollider = GetComponent<BoxCollider2D>();
+        collider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         float moveInput = Input.GetAxisRaw("P2Horizontal");
-
-        //transform.position += new Vector3(moveInput, 0, 0) * Time.deltaTime * speed * 10f;
         rb.velocity = new Vector2(moveInput * speed * 10f, rb.velocity.y);
         if (moveInput > 0)
         {
@@ -41,72 +33,55 @@ public class CharacterController2 : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
-
         animator.SetFloat("Speed", Mathf.Abs(moveInput));
 
-
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-        if (isGrounded && Input.GetButton("P2Jump"))
+        if (isGrounded && Input.GetButtonDown("P2Jump"))
         {
             rb.velocity = Vector2.up * jumpForce;
             animator.SetBool("IsJumping", true);
             audio.Play();
         }
-
-
-       
-        isUnderCeiling = Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround);
-
-        if (isGrounded && Input.GetButton("P2Crouch"))
-                    {
-            animator.SetBool("IsCrouching", true);
-            crouchDisableCollider.enabled = false;
-
-            isCrouching = true;
-
+        if (Physics2D.OverlapCircle(feetPos.position, checkRadius).CompareTag("Floor"))
+        {
+            gameObject.transform.parent = Physics2D.OverlapCircle(feetPos.position, checkRadius).transform;
+            //GetComponent<CapsuleCollider2D>().sharedMaterial.friction = 1;
         }
         else
         {
-            isCrouching = false;
+            gameObject.transform.parent = null;
+            //GetComponent<CapsuleCollider2D>().sharedMaterial.friction = 0;
         }
 
-        if (isCrouching && isUnderCeiling)
+        if (rb.velocity.y < 0 && !isGrounded)
         {
-            animator.SetBool("IsCrouching", true);
-            crouchDisableCollider.enabled = false;
-
-            isCrouching = true;
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
         }
-
-        else if (!isCrouching && !isUnderCeiling)
-        {
-            animator.SetBool("IsCrouching", false);
-            crouchDisableCollider.enabled = true;
-
-            isCrouching = false;
-        }
-
-    
-       if (Physics2D.OverlapCircle(feetPos.position, checkRadius).CompareTag("Floor"))
-       {
-                gameObject.transform.parent = Physics2D.OverlapCircle(feetPos.position, checkRadius).transform;
-                //GetComponent<CapsuleCollider2D>().sharedMaterial.friction = 1;
-       }
-       else
-       {
-                gameObject.transform.parent = null;
-                //GetComponent<CapsuleCollider2D>().sharedMaterial.friction = 0;
-       }
-
-       if (rb.velocity.y < 0 && !isGrounded)
-            {
-                animator.SetBool("IsJumping", false);
-                animator.SetBool("IsFalling", true);
-            }
         else/* if (rb.velocity.y >= 0)*/
-            {
-                animator.SetBool("IsFalling", false);
-                //animator.SetBool("IsJumping", false);
-            }
+        {
+            animator.SetBool("IsFalling", false);
+            //animator.SetBool("IsJumping", false);
+        }
+
+        if (isGrounded && Input.GetButton("P2Crouch"))
+        {
+            collider.size = new Vector2(collider.size.x, 0.5f);
+            collider.offset = new Vector2(collider.offset.x, -0.55f);
+            animator.SetBool("IsCrouching", true);
+        }
+        else
+        {
+            collider.size = new Vector2(collider.size.x, 1.4f);
+            collider.offset = new Vector2(collider.offset.x, -0.3f);
+            animator.SetBool("IsCrouching", false);
+        }
+        isUnderBlock = Physics2D.OverlapCircle(headPos.position, checkRadius, whatIsGround);
+        if (isUnderBlock)
+        {
+            collider.size = new Vector2(collider.size.x, 0.5f);
+            collider.offset = new Vector2(collider.offset.x, -0.55f);
+            animator.SetBool("IsCrouching", true);
         }
     }
+}
--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -4,50 +4,45 @@ public class characterController : MonoBehaviour
 {
     float speed = 1f, checkRadius = 0.2f, jumpForce = 20f;
     public bool isGrounded;
+    public bool isUnderBlock;
     public Transform feetPos;
+    public Transform headPos;
     public LayerMask whatIsGround;
     public Animator animator;
     public AudioSource audio;
     Rigidbody2D rb;
+    public CapsuleCollider2D collider;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         audio = GetComponent<AudioSource>();
+        collider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         float moveInput = Input.GetAxisRaw("P1Horizontal");
-        if (Input.GetAxisRaw("P1Horizontal") == Input.GetAxisRaw("P2Horizontal")/* && Input.GetAxisRaw("P1Horizontal") != 0*/)
-        {
-            //transform.position += new Vector3(moveInput, 0, 0) * Time.deltaTime * speed * 10f;
-            rb.velocity = new Vector2(moveInput * speed * 10f, rb.velocity.y);
-            if (moveInput > 0)
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-            }
-            else if (moveInput < 0)
-            {
-                transform.eulerAngles = new Vector3(0, 180, 0);
-            }
 
-            animator.SetFloat("Speed", Mathf.Abs(moveInput));
-        }
-        else
+        rb.velocity = new Vector2(moveInput * speed * 10f, rb.velocity.y);
+        if (moveInput > 0)
         {
-            rb.velocity = new Vector2(0, rb.velocity.y);
+            transform.eulerAngles = new Vector3(0, 0, 0);
         }
+        else if (moveInput < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        animator.SetFloat("Speed", Mathf.Abs(moveInput));
 
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
-        if (isGrounded && Input.GetButton("P1Jump") && Input.GetButtonDown("P2Jump") || isGrounded && Input.GetButtonDown("P1Jump") && Input.GetButton("P2Jump"))
+        if (isGrounded && Input.GetButtonDown("P1Jump"))
         {
             rb.velocity = Vector2.up * jumpForce;
             animator.SetBool("IsJumping", true);
             audio.Play();
         }
-
         if (Physics2D.OverlapCircle(feetPos.position, checkRadius).CompareTag("Floor"))
         {
             gameObject.transform.parent = Physics2D.OverlapCircle(feetPos.position, checkRadius).transform;
@@ -68,6 +63,27 @@ public class characterController : MonoBehaviour
         {
             animator.SetBool("IsFalling", false);
             //animator.SetBool("IsJumping", false);
+        }
+
+        if (isGrounded && Input.GetButton("P1Crouch"))
+        {
+            collider.size = new Vector2(collider.size.x, 0.5f);
+            collider.offset = new Vector2(collider.offset.x, -0.55f);
+            animator.SetBool("IsCrouching", true);
+        }
+        else
+        {
+            collider.size = new Vector2(collider.size.x, 1.4f);
+            collider.offset = new Vector2(collider.offset.x, -0.3f);
+            animator.SetBool("IsCrouching", false);
+        }
+
+        isUnderBlock = Physics2D.OverlapCircle(headPos.position, checkRadius, whatIsGround);
+        if (isUnderBlock)
+        {
+            collider.size = new Vector2(collider.size.x, 0.5f);
+            collider.offset = new Vector2(collider.offset.x, -0.55f);
+            animator.SetBool("IsCrouching", true);
         }
     }
 }
